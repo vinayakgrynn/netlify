7c134cfccd6b45eda86572d23695ac0ae870bd6f
Vinayak Patel <61875581+vinayakgrynn@users.noreply.github.com>
2020-07-29 09:07:39 -0400

    Add files via upload

--- functions/shop.js
+++ functions/shop.js
@@ -3,20 +3,20 @@ import querystring from "querystring";
 exports.handler = (event, context, callback) => {
     try {
         // Only allow POST
-        if (event.httpMethod !== "POST") {
-            return { statusCode: 200, body: "Method Not Allowed" };
-        }
+        //if (event.httpMethod !== "POST") {
+            //return { statusCode: 200, body: "Method Not Allowed" };
+        //}
 
         // When the method is POST, the name will no longer be in the event’s
         // queryStringParameters – it’ll be in the event body encoded as a query string
         //const params = querystring.parse(event.body);
         //const name = params.name || "World";
-        var obj = JSON.parse(event.body);
-        //const name = event.queryStringParameters.name || "World";
+        //var obj = JSON.parse(event.body);
+        const name = event.queryStringParameters.name || "World";
 
         callback(null, {
             statusCode: 200,
-            body: obj
+            body: name
         });
     }
     catch(err) {
